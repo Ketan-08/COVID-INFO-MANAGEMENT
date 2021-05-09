00885ef97e47d9f712ab8ed0bdc64cb8b9ee99e7
Ketan-08 <78249619+Ketan-08@users.noreply.github.com>
2021-05-09 11:09:40 +0530

    Add files via upload

---
+++ bedavail.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+class Bed:
+    def __init__(self):
+            
+        window2=Tk()
+        window2.geometry("500x500")
+        window2.title('HOSPITAL BED AVAILABILITY')
+        window2.mainloop()
---
+++ oxygen.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+class Oxygen:
+    def __init__(self):
+            
+        window3=Tk()
+
+
+
+
+
+
+        
+        window3.geometry("500x500")
+        window3.title('HOSPITAL BED AVAILABILITY')
+        window3.mainloop()
+        

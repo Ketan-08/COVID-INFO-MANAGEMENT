0e192c4374eaf16077ead22fb6b2ac00eb8bf0da
Ketan-08 <78249619+Ketan-08@users.noreply.github.com>
2021-05-12 18:04:41 +0530

    Delete bedavail.py

--- bedavail.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-
-class Bed:
-    def __init__(self):
-            
-        window2=Tk()
-        window2.geometry("500x500")
-        window2.title('HOSPITAL BED AVAILABILITY')
-        window2.mainloop()
